0a0ddab423f452f54605c67ed0a6302bf058439b
Iveth Alejandra Meza Moreno <iveth.meza@coppel.com>
2019-02-06 17:50:52 -0700

    Spare Juego boliche

--- src/app/juego-boliche/juego-boliche.component.spec.ts
+++ src/app/juego-boliche/juego-boliche.component.spec.ts
@@ -45,6 +45,27 @@ it('Deberia obtener 0 puntos', () => {
   expect(puntos).toBe(0);
 });
 
+it('Debe obtener 150 puntos', () => {
+  const juegoBoliche = new JuegoBoliche();
+  const tiros = 21;
+  const pinos = 5;
+
+  jugarLinea(tiros, juegoBoliche, pinos);
+  const puntos = juegoBoliche.obtenerPuntuacion();
+  expect(puntos).toBe(150);
+});
+
+it('Debe obtener 155 puntos', () => {
+  const juegoBoliche = new JuegoBoliche();
+  const tiros = 20;
+  const pinos = 5;
+
+  jugarLinea(tiros, juegoBoliche, pinos);
+  juegoBoliche.tirar(10);
+  const puntos = juegoBoliche.obtenerPuntuacion();
+  expect(puntos).toBe(155);
+});
+
 function jugarLinea(tiros: number, juegoBoliche: JuegoBoliche, pinos: number) {
   for (let i = 0; i < tiros; i++) {
     juegoBoliche.tirar(pinos);
--- src/app/juego-boliche/juegoBoliche.ts
+++ src/app/juego-boliche/juegoBoliche.ts
@@ -1,11 +1,27 @@
 export class JuegoBoliche {
     private puntosTotales = 0;
+    private tiros: number[] = [];
 
     tirar(pinos: number) {
-        this.puntosTotales += pinos;
+        this.tiros.push(pinos);
     }
 
     obtenerPuntuacion(): number {
+
+        let index = 0;
+        let entradas = 1;
+        while (entradas <= 10) {
+
+            if ((this.tiros[index] + this.tiros[index + 1]) === 10) {
+                this.puntosTotales += 10 + this.tiros[index + 2];
+            } else {
+                this.puntosTotales += this.tiros[index] + this.tiros[index + 1];
+            }
+
+            index += 2;
+            entradas++;
+
+        }
         return this.puntosTotales;
     }
 
