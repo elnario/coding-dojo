87c5be9ce3e9f736cca0a3abf0ff70b5a8255aab
Iveth Alejandra Meza Moreno <iveth.meza@coppel.com>
2019-02-13 17:22:47 -0700

    Kata terminada

--- src/app/juego-boliche/juego-boliche.component.spec.ts
+++ src/app/juego-boliche/juego-boliche.component.spec.ts
@@ -66,6 +66,63 @@ it('Debe obtener 155 puntos', () => {
   expect(puntos).toBe(155);
 });
 
+it('Debe obtener 300 puntos', () => {
+  const juegoBoliche = new JuegoBoliche();
+  const tiros = 12;
+  const pinos = 10;
+
+  jugarLinea(tiros, juegoBoliche, pinos);
+
+  const puntos = juegoBoliche.obtenerPuntuacion();
+  expect(puntos).toBe(300);
+
+});
+
+
+it('Debe obtener 299 puntos', () => {
+  const juegoBoliche = new JuegoBoliche();
+  const tiros = 11;
+  const pinos = 10;
+
+  jugarLinea(tiros, juegoBoliche, pinos);
+  juegoBoliche.tirar(9);
+  const puntos = juegoBoliche.obtenerPuntuacion();
+  expect(puntos).toBe(299);
+
+});
+
+
+it('Debe obtener 120 puntos', () => {
+
+  const juegoBoliche = new JuegoBoliche();
+
+  juegoBoliche.tirar(10);
+  juegoBoliche.tirar(4);
+  juegoBoliche.tirar(3);
+  juegoBoliche.tirar(5);
+  juegoBoliche.tirar(5);
+  juegoBoliche.tirar(9);
+  juegoBoliche.tirar(1);
+  juegoBoliche.tirar(0);
+  juegoBoliche.tirar(3);
+  juegoBoliche.tirar(6);
+  juegoBoliche.tirar(4);
+  juegoBoliche.tirar(0);
+  juegoBoliche.tirar(10);
+  juegoBoliche.tirar(5);
+  juegoBoliche.tirar(5);
+  juegoBoliche.tirar(2);
+  juegoBoliche.tirar(5);
+  juegoBoliche.tirar(10);
+  juegoBoliche.tirar(4);
+  juegoBoliche.tirar(6);
+
+  const puntos = juegoBoliche.obtenerPuntuacion();
+  expect(puntos).toBe(120);
+
+});
+
+
 function jugarLinea(tiros: number, juegoBoliche: JuegoBoliche, pinos: number) {
   for (let i = 0; i < tiros; i++) {
     juegoBoliche.tirar(pinos);
--- src/app/juego-boliche/juegoBoliche.ts
+++ src/app/juego-boliche/juegoBoliche.ts
@@ -12,17 +12,29 @@ export class JuegoBoliche {
         let entradas = 1;
         while (entradas <= 10) {
 
-            if ((this.tiros[index] + this.tiros[index + 1]) === 10) {
+            if (this.esChuza(index)) {
+                this.puntosTotales += 10 + this.tiros[index + 1] + this.tiros[index + 2];
+                index += 1;
+            } else if (this.esSpare(index)) {
                 this.puntosTotales += 10 + this.tiros[index + 2];
+                index += 2;
             } else {
                 this.puntosTotales += this.tiros[index] + this.tiros[index + 1];
+                index += 2;
             }
 
-            index += 2;
             entradas++;
 
         }
         return this.puntosTotales;
     }
 
+
+    private esSpare(index: number) {
+        return (this.tiros[index] + this.tiros[index + 1]) === 10;
+    }
+
+    private esChuza(index: number) {
+        return this.tiros[index] === 10;
+    }
 }
