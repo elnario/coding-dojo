6e7a2d8b4f20c19b198062d8d1d67ee945631944
Christian Medina <el.nario@gmail.com>
2019-03-08 01:50:04 -0700

    Actualizar dependencias
    Actualizar dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { JuegoBolicheComponent } from './juego-boliche/juego-boliche.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    JuegoBolicheComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, FormsModule
