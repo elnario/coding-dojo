177a71278eaba6fd7ac0495b2d7179c5110883b6
Iveth Alejandra Meza Moreno <iveth.meza@coppel.com>
2019-04-03 17:34:50 -0700

    Coding Dojo 3 de Abril 2019

--- src/app/trading-card-game/juego.spec.ts
+++ src/app/trading-card-game/juego.spec.ts
@@ -30,4 +30,70 @@ describe('trading-card-game-juego', () => {
 
   });
 
+  it('El jugador activo debe Iniciar con 3 cartas.', () => {
+
+    spyOn(juego.jugadorActivo, 'robarCarta').and.callThrough();
+    juego.iniciarJuego();
+    expect(juego.jugadorActivo.robarCarta).toHaveBeenCalledTimes(3);
+    expect(juego.jugadorActivo.mano.length).toBe(3);
+    expect(juego.jugadorActivo.mazo.length).toBe(17);
+  });
+
+  it('El oponente debe Iniciar con 4 cartas.', () => {
+
+    spyOn(juego.oponente, 'robarCarta').and.callThrough();
+    juego.iniciarJuego();
+    expect(juego.oponente.robarCarta).toHaveBeenCalledTimes(4);
+    expect(juego.oponente.mano.length).toBe(4);
+    expect(juego.oponente.mazo.length).toBe(16);
+  });
+
+  it('Al iniciar un turno, aumenta el maximo de mana del jugador activo.', () => {
+
+    spyOn(juego.oponente, 'robarCarta').and.callThrough();
+    juego.iniciarJuego();
+    expect(juego.jugadorActivo.contadores_mana).toBe(0);
+
+    for (let i = 0; i < 10; i++) {
+      juego.iniciarTurno();
+      expect(juego.jugadorActivo.contadores_mana).toBe(i + 1);
+    }
+
+
+  });
+
+  it('Un jugador puede tener maximo 10 contadores de mana.', () => {
+    juego.jugadorActivo.contadores_mana = 10;
+
+    for (let i = 0; i < 10; i++) {
+      juego.iniciarTurno();
+      expect(juego.jugadorActivo.contadores_mana).toBe(10);
+    }
+  });
+
+  it('Al inicar un turno rellena el mana del jugador activo al maximo.', () => {
+
+    for (let i = 0; i < 10; i++) {
+      juego.iniciarTurno();
+      expect(juego.jugadorActivo.mana).toBe(juego.jugadorActivo.contadores_mana);
+    }
+  });
+
+  it('Al inicar un turno el jugador activo roba una carta.', () => {
+    spyOn(juego.jugadorActivo, 'robarCarta').and.callThrough();
+    juego.iniciarTurno();
+    expect(juego.jugadorActivo.robarCarta).toHaveBeenCalled();
+  });
+
+  it('Al finalizar un turno se deben intercambiar el jugador activo y el oponente.', () => {
+    const jugador1 = juego.jugadorActivo;
+    const jugador2 = juego.oponente;
+
+    juego.finalizarTurno();
+    expect(juego.jugadorActivo).toEqual(jugador2);
+    expect(juego.oponente).toEqual(jugador1);
+
+  });
+
+
 });
--- src/app/trading-card-game/juego.ts
+++ src/app/trading-card-game/juego.ts
@@ -12,5 +12,27 @@ export class Juego {
     iniciarJuego() {
         this.jugadorActivo.barajarMazo();
         this.oponente.barajarMazo();
+
+        for (let i = 0; i < 3; i++) {
+            this.jugadorActivo.robarCarta();
+        }
+
+        for (let i = 0; i < 4; i++) {
+            this.oponente.robarCarta();
+        }
+    }
+
+    iniciarTurno() {
+        if (this.jugadorActivo.contadores_mana < 10) {
+            this.jugadorActivo.contadores_mana++;
+        }
+        this.jugadorActivo.mana = this.jugadorActivo.contadores_mana;
+        this.jugadorActivo.robarCarta();
+    }
+
+    finalizarTurno(): any {
+        const jugador = this.jugadorActivo;
+        this.jugadorActivo = this.oponente;
+        this.oponente = jugador;
     }
 }
